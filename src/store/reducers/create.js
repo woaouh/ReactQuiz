c018c841b15b3996ba39aaa726393297761379d3
Vladimir <woaouh@outlook.com>
2020-06-01 22:10:50 +0300

    Finished Reduxing Quiz-Create page

---
+++ src/store/reducers/create.js
@@ -0,0 +1,25 @@
+import {
+  RESET_QUIZ_CREATION,
+  CREATE_QUIZ_QUESTION,
+} from '../actions/actionTypes';
+
+const initialState = {
+  quiz: [],
+};
+
+export default function createReducer(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_QUIZ_QUESTION:
+      return {
+        ...state,
+        quiz: [...state.quiz, action.item],
+      };
+    case RESET_QUIZ_CREATION:
+      return {
+        ...state,
+        quiz: [],
+      };
+    default:
+      return state;
+  }
+}
